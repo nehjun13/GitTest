9dc6454310c275aba4eaecde695f59cd8297168c
nehajun13 <nehajun13@gmail.com>
2018-02-25 18:04:12 +0530

    full commit

---
+++ src/test/java/gitTest/GitTest.java
@@ -0,0 +1,24 @@
+package gitTest;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GitTest {
+
+@Test
+public void logintest() throws IOException {
+WebDriver wd = new FirefoxDriver();
+wd.manage().window().maximize();
+wd.get("https://www.google.co.in/");
+File src= ((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+FileUtils.copyFile(src, new File(("C:\\Users\\Myra\\eclipse-workspace\\GitTest\\Screenshot\\Google.jpeg")));
+}
+
+}
